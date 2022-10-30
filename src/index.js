7bd2b943132f491e81c7bf7177b6bce5b09e7ee3
Sergey87 <serge.techno@gmail.com>
2022-10-30 23:53:42 +0300

    added unique key from geo.ipify and input processing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('test');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  // data validation
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_pUbah6gQCSS11WTILAD4wJIyZHVzL&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
