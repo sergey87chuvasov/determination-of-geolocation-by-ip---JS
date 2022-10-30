c0c6ceee8b732341985fb70081f02215d7c18080
Sergey87 <serge.techno@gmail.com>
2022-10-31 00:44:45 +0300

    added validation of entered values and work with DOM

---
+++ src/helpers/validate-ip.js
@@ -0,0 +1,11 @@
+export function validatIp(ip) {
+  if (
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(
+      ip
+    )
+  ) {
+    return true;
+  }
+  alert('You have entered an invalid IP address!');
+  return false;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,41 @@
+import { validatIp } from './helpers';
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timeZoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 function getData() {
   // data validation
-  fetch(
-    `https://geo.ipify.org/api/v2/country?apiKey=at_pUbah6gQCSS11WTILAD4wJIyZHVzL&ipAddress=${ipInput.value}`
-  )
-    .then((response) => response.json())
-    .then(console.log);
+  if (validatIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country?apiKey=at_pUbah6gQCSS11WTILAD4wJIyZHVzL&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      // ...send our data in function setInfo
+      // .then((data) => setInfo(data));
+      .then(setInfo);
+  }
 }
 
 function handleKey(e) {
   if (e.key === 'Enter') {
     getData();
   }
+}
+
+// we get object
+function setInfo(mapData) {
+  // console.log(mapData);
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText =
+    mapData.location.country + ' ' + mapData.location.region;
+  timeZoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
